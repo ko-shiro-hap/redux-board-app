6cc3259e2ac88f77362a84e25ea7e71059fc24f6
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-13 20:09:29 +0900

    feature:掲示板の見た目作成

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ function App() {
   const postList = useSelector((state) => state.posts.value);
   console.log(postList);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+      <div className="addPost">
+        <input type="text" placeholder="お名前" />
+        <input type="text" placeholder="投稿内容" />
+        <button>投稿</button>
+        <hr />
+      </div>
+    </div>
+  );
 }
 
 export default App;
